e9da762f4a04db2a73df280405ebf149b9d05c03
unknown <numton2503@gmail.com>
2024-10-13 22:01:42 +0700

    update

--- main.js
+++ main.js
@@ -59,7 +59,8 @@ const generateToken = (user) => {
 
 // ฟังก์ชัน Middleware สำหรับการตรวจสอบ JWT token
 const authenticateToken = (req, res, next) => {
-    const token = req.cookies['token'];
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // แยก Bearer ออก
     if (!token) {
         console.log('No token provided');
         return res.sendStatus(401); 
